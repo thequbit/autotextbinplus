a31769003cbe2448c485aa5b484fd4b3cd79f0b7
thequbit <thequbit@gmail.com>
2015-04-25 19:45:46 -0400

    working prediction with test

--- autotextbinplus/bintexts.py
+++ autotextbinplus/bintexts.py
@@ -9,6 +9,9 @@ import re
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+
+from sklearn.naive_bayes import MultinomialNB
 
 import glob
 
@@ -30,49 +33,114 @@ def clean_text(text, language='english'):
 
     return cleaned_text
 
-def build_bag_of_words(ctexts, build_dist=False):
+def build_bag_of_words(ctexts): #, build_dist=False):
 
-    # Initalize our TF-IDF vectorizer
-    vectorizer = TfidfVectorizer(min_df=1)
+    print type(ctexts)
 
-    # perform the transform using the TF-IDF values
-    data_features = vectorizer.fit_transform(ctexts) #.toarray()
+    count_vect = CountVectorizer()
+    counts = count_vect.fit_transform(ctexts)
 
-    dist = None
-    if build_dist == True:
-       dist = []
-       _d = np.sum(data_features.toarray(), axis=0)
-       vocab = vectorizer.get_feature_names()
-       for tag, count in zip(vocab, _d):
-           dist.append((count, tag))
-    
-    return data_features, dist
+    #tfidf_vect = TfidfVectorizer()
+    #features = tfidf_vect.fit_transform(ctexts) #.toarray()
+
+    tfidf_trans = TfidfTransformer()
+    features = tfidf_trans.fit_transform(counts)
+
+    #dist = None
+    #if build_dist == True:
+    #   dist = []
+    #   _d = np.sum(data_features.toarray(), axis=0)
+    #   vocab = vectorizer.get_feature_names()
+    #   for tag, count in zip(vocab, _d):
+    #       dist.append((count, tag))
+   
+    return count_vect, tfidf_trans, counts, features
 
 def calculate_vector(texts):
     
     ctexts = []
-    raw_word_count = 0
-    clean_word_count = 0
+    #raw_word_count = 0
+    #clean_word_count = 0
     for text in texts:
         ctext = clean_text(text)
         ctexts.append(ctext)
-        raw_word_count += len(text.split())
-        clean_word_count += len(ctext.split())
-    bow, dist = build_bag_of_words(ctexts, build_dist=True)
+        #raw_word_count += len(text.split())
+        #clean_word_count += len(ctext.split())
+    count_vect, tfidf_trans, counts, features = build_bag_of_words(ctexts) #, build_dist=True)
 
-    return bow, dist, raw_word_count, clean_word_count
+    return count_vect, tfidf_trans, counts, features, ctexts
+
+def train_classifier(counts, features, ctexts, categories):
+
+    classifier = MultinomialNB().fit(features, categories)
+
+    return classifier
+
+def predict(count_vect, tfidf_trans, classifier, text):
+
+    ctexts = [clean_text(text)]
+
+    #print type(ctexts)
+
+    #count_vect = CountVectorizer()
+    counts = count_vect.transform(ctexts)
+    
+    #fidf_vect = TfidfVectorizer(min_df=1)
+    features = tfidf_trans.fit_transform(counts) #.toarray()
+
+    predicted = classifier.predict(features)
+
+    return predicted
 
 if __name__ == '__main__':
 
+    # get doc files
     doc_files = glob.glob('./data/*.txt')
 
+    #
+    # define our prefixes for our different doc categories
+    #                0    1     2     3
+    #
+    doc_prefixes = ['tb','pb', 'ps', 'hp']
+
     texts = []
+    categories = []
     for doc_file in doc_files:
         with open(doc_file,'r') as f:
-            texts.append(f.read())
+            for cat in doc_prefixes:
+                if cat in doc_file:
+                    print 'file: {0}, cat: {1}, index: {2}'.format(doc_file, cat, doc_prefixes.index(cat))
+                    texts.append(f.read()[:1024])
+                    categories.append(doc_prefixes.index(cat))
+                    break
 
-    bow, dist, rwc, cwc = calculate_vector(texts)
+    # perform TF-IDF vectorize on each category
+    count_vect, tfidf_trans, counts, features, ctexts = calculate_vector(texts)
 
-    print "corpus size: {0}, raw word count: {1}, clean word count: {2}".format(len(texts), rwc, cwc)
+    # train based on vectors and categories
+    classifier = train_classifier(counts, features, ctexts, categories)
+
+    correct_count = 0
+    for doc_file in doc_files:
+        with open('./data/ps-doc-2.txt') as f:
+            for cat in doc_prefixes:
+                if cat in doc_file:
+                    text = f.read()[:1024]
+                    cat_val = doc_prefixes.index(cat)
+     
+                    # perform prediction
+                    predicted = predict(count_vect, tfidf_trans, classifier, text)
+
+                    correct = False
+                    if predicted[0] == cat_val:
+                        correct_count += 1
+                        correct = True
+
+                    print "file: {0}, cat: {1}, index: {2}, predicted: {3}, correct: {4}".format(doc_file, cat, cat_val, predicted, correct)
+
+                    break
+
+    print "{0}/{1} Correct.".format(correct_count, len(doc_files))
+
     print 'done'
 
