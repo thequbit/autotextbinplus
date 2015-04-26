ce175b7f5d1f25aef76a236b8d81ab4a5bf990c7
thequbit <thequbit@gmail.com>
2015-04-26 11:50:02 -0400

    transitioned to using yapot

--- autotextbinplus/get-data-set.py
+++ autotextbinplus/get-data-set.py
@@ -4,7 +4,7 @@ from subprocess import call
 from barking_owl import Scraper
 import datetime
 import requests
-#from yapot import convert_document
+from yapot import convert_document
 from unpdfer import Unpdfer
 
 class DocGrabber(object):
@@ -25,24 +25,24 @@ class DocGrabber(object):
                     break
                 f.write(block)
         print "PDF Downloaded."
-        #success, pdf_text = convert_document(
-        #    pdf_filename = 'file.pdf',
-        #    delete_files = True,
-        #    page_delineation = '\n',
-        #    verbose = True,
-        #    make_thumbs = False,
-        #)
+        success, pdf_text = convert_document(
+            pdf_filename = 'doc.pdf',
+            delete_files = True,
+            page_delineation = '\n',
+            verbose = True,
+            make_thumbs = False,
+        )
         #u = Unpdfer()
         #created, pdftext, pdfhash, success = u.unpdf(
         #    filename = 'doc.pdf',
         #    SCRUB = False,
         #    verbose = True,
         #)
-        call(['pdftotext','doc.pdf','./data/{0}doc-{1}.txt'.format(self.prefix, self.doc_count)])
+        #call(['pdftotext','doc.pdf','./data/{0}doc-{1}.txt'.format(self.prefix, self.doc_count)])
         #if success == False:
         #    raise Exception('invalid pdf')
-        #with open('doc-{0}.txt'.format(self.do_count),'w') as f:
-        #    f.write(pdftext)
+        with open('./data/{0}doc-{1}.txt'.format(self.prefix, self.doc_count),'w') as f:
+            f.write(pdf_text)
         os.remove('doc.pdf')
         self.doc_count += 1
 
@@ -84,12 +84,19 @@ if __name__ == '__main__':
     ]
     '''
 
+    
     urls = [
         ('tb-', 'http://www.townofbrighton.org/Archive.aspx?AMID=85'), # town board
         ('pb-', 'http://www.townofbrighton.org/Archive.aspx?AMID=69'), # zoning board
         ('ps-', 'http://www.townofbrighton.org/Archive.aspx?AMID=72'), # public safety board
         ('hp-', 'http://www.townofbrighton.org/Archive.aspx?AMID=89'), # historic preservation board
     ]
+    
+    '''
+    urls = [
+        ('td-', 'http://timduffy.me/'),
+    ]
+    '''
 
     for prefix,url in urls:
         dg = DocGrabber()
