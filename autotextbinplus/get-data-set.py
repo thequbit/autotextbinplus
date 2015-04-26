3279b12440ad457ead05cd2943f2ccb367a113e5
thequbit <thequbit@gmail.com>
2015-04-26 13:09:53 -0400

    added henrietta training set

--- autotextbinplus/get-data-set.py
+++ autotextbinplus/get-data-set.py
@@ -5,7 +5,7 @@ from barking_owl import Scraper
 import datetime
 import requests
 from yapot import convert_document
-from unpdfer import Unpdfer
+#from unpdfer import Unpdfer
 
 class DocGrabber(object):
 
@@ -25,29 +25,30 @@ class DocGrabber(object):
                     break
                 f.write(block)
         print "PDF Downloaded."
-        success, pdf_text = convert_document(
-            pdf_filename = 'doc.pdf',
-            delete_files = True,
-            page_delineation = '\n',
-            verbose = True,
-            make_thumbs = False,
-        )
+        #success, pdf_text = convert_document(
+        #    pdf_filename = 'doc.pdf',
+        #    delete_files = True,
+        #    page_delineation = '\n',
+        #    verbose = True,
+        #    make_thumbs = False,
+        #)
         #u = Unpdfer()
         #created, pdftext, pdfhash, success = u.unpdf(
         #    filename = 'doc.pdf',
         #    SCRUB = False,
         #    verbose = True,
         #)
-        #call(['pdftotext','doc.pdf','./data/{0}doc-{1}.txt'.format(self.prefix, self.doc_count)])
+        call(['pdftotext','doc.pdf','./{0}/{1}doc-{2}.txt'.format(self.directory, self.prefix, self.doc_count)])
         #if success == False:
         #    raise Exception('invalid pdf')
-        with open('./data/{0}doc-{1}.txt'.format(self.prefix, self.doc_count),'w') as f:
-            f.write(pdf_text)
+        #with open('./{0}/{1}doc-{2}.txt'.format(self.directory, self.prefix, self.doc_count),'w') as f:
+        #    f.write(pdf_text)
         os.remove('doc.pdf')
         self.doc_count += 1
 
-    def scrape(self, url, prefix, level=1):
+    def scrape(self, directory, url, prefix, level=1):
 
+        self.directory = directory
         self.prefix = prefix
 
         s = Scraper(DEBUG=True)
@@ -76,29 +77,36 @@ class DocGrabber(object):
 
 if __name__ == '__main__':
 
-    ''' # henrietta
+    
+    # henrietta
+    directory = 'henrietta'
     urls = [
         ('tb-', 'http://henrietta.org/index.php/2012-05-16-11-50-52/town-board/agendas-minutes/2014-agenda-a-minutes-2'),
         ('zb-', 'http://henrietta.org/index.php/2012-05-16-11-50-52/2012-05-16-12-27-7/agendas-minutes/zoning-board-2014'),
         ('cb-', 'http://henrietta.org/index.php/2012-05-16-11-50-52/2012-05-16-12-27-8/agendas-minutes/2014-agenda-a-minutes'),
     ]
-    '''
-
     
+
+    '''
+    directory = 'brighton'
     urls = [
         ('tb-', 'http://www.townofbrighton.org/Archive.aspx?AMID=85'), # town board
         ('pb-', 'http://www.townofbrighton.org/Archive.aspx?AMID=69'), # zoning board
         ('ps-', 'http://www.townofbrighton.org/Archive.aspx?AMID=72'), # public safety board
         ('hp-', 'http://www.townofbrighton.org/Archive.aspx?AMID=89'), # historic preservation board
     ]
-    
     '''
+
+    '''
+    directory = 'timduffy.me'
     urls = [
         ('td-', 'http://timduffy.me/'),
     ]
-    '''
+    '''    
 
+    if not os.path.exists(directory):
+        os.makedirs(directory)
     for prefix,url in urls:
         dg = DocGrabber()
-        dg.scrape(url, prefix=prefix, level=1)
+        dg.scrape(directory, url, prefix=prefix, level=1)
 
---
+++ autotextbinplus/henrietta/README.md
@@ -0,0 +1,11 @@
+Files from:
+
+'''
+    directory = 'henrietta'
+    urls = [
+        ('tb-', 'http://henrietta.org/index.php/2012-05-16-11-50-52/town-board/agendas-minutes/2014-agenda-a-minutes-2'),
+        ('zb-', 'http://henrietta.org/index.php/2012-05-16-11-50-52/2012-05-16-12-27-7/agendas-minutes/zoning-board-2014'),
+        ('cb-', 'http://henrietta.org/index.php/2012-05-16-11-50-52/2012-05-16-12-27-8/agendas-minutes/2014-agenda-a-minutes'),
+    ]
+'''
+
